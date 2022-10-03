084b43d263e22680ae357dfb29d73011ed623078
Arman-Kabir <armankabir8@gmail.com>
2022-10-03 23:10:37 +0600

    exploring objects

--- objects.js
+++ objects.js
@@ -1,24 +1,14 @@
 
-const circle = {
-    radius:1,
-    location:{
-        x:1,
-        y:1
-    },
-    draw:function(){
-        console.log('method:::: draw');
-    }
-};
 
-const circle = {
-    radius:1,
-    location:{
-        x:1,
-        y:1
-    },
-    draw:function(){
-        console.log('method:::: draw');
-    }
-};
+function createCircle(radius) {
+
+    return = {
+        radius: 1,
+        location: {
+            x: 1,
+            y: 1
+        }
+    };
+}
 
 circle.draw();
