287620bcc1dd38482c699febb7fe81fba5a2b02f
Arman-Kabir <armankabir8@gmail.com>
2022-10-04 15:00:09 +0600

    exploring objects1

--- objects.js
+++ objects.js
@@ -1,26 +1,29 @@
 
 // Factory function
-function createCircle(radius) {
+// function createCircle(radius) {
 
-    return {
-        radius,
-        draw: function () {
-            console.log('draw');
-        }
-    };
-}
+//     return {
+//         radius,
+//         draw: function () {
+//             console.log('draw');
+//         }
+//     };
+// }
 
-const circle = createCircle(1);
+// const circle = createCircle(1);
 
-// circle.draw();
+// // circle.draw();
 
-// constructor function
-function Circle(radius) {
-    console.log('this', this);
-    this.radius = radius;
-    this.draw = function () {
-        console.log('draw');
-    }
-}
+// // constructor function
+// function Circle(radius) {
+//     console.log('this', this);
+//     this.radius = radius;
+//     this.draw = function () {
+//         console.log('draw');
+//     }
+// }
 
-const another = new Circle(1);
+// const another = new Circle(1);
+
+let x = {};
+// let x = new Object();
