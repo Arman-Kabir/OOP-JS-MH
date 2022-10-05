6fbe0a13b7bf5484cb73f03330b48498c7781637
Arman-Kabir <armankabir8@gmail.com>
2022-10-05 09:31:48 +0600

    constructor-property done

---
+++ constructor-property.js
@@ -0,0 +1,30 @@
+// let x = {};
+
+// let x = new Object();
+
+
+
+
+// Functions are objects ...26m
+
+
+// constructor function
+function Circle(radius) {
+    console.log('this', this);
+    this.radius = radius;
+    this.draw = function () {
+        console.log('draw');
+    }
+}
+
+const Circle1 = new Function('radius', `
+this.radius = radius;
+    this.draw = function () {
+        console.log('draw');
+    }
+`);
+const circle = new Circle1(1);
+
+const another = new Circle(1);
+
+
