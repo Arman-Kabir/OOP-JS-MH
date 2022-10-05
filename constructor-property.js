8609e752a972c5738d38ab45e92c71b7fb644d19
Arman-Kabir <armankabir8@gmail.com>
2022-10-05 17:47:35 +0600

    add/delete properties done

--- constructor-property.js
+++ constructor-property.js
@@ -2,28 +2,45 @@
 // Functions are objects ...26m
 
 // constructor function
+// function Circle(radius) {
+//     console.log('this', this);
+//     this.radius = radius;
+//     this.draw = function () {
+//         console.log('draw');
+//     }
+// }
+
+// const Circle1 = new Function('radius', `
+// this.radius = radius;
+//     this.draw = function () {
+//         console.log('draw');
+//     }
+// `);
+
+// const circle = new Circle1(1);
+
+// const another = new Circle(1);
+
+
+
+// adding/removing property
+
 function Circle(radius) {
-    console.log('this', this);
+    // console.log('this', this);
     this.radius = radius;
     this.draw = function () {
         console.log('draw');
     }
 }
 
-const Circle1 = new Function('radius', `
-this.radius = radius;
-    this.draw = function () {
-        console.log('draw');
-    }
-`);
+const circle = new Circle(10);
 
-const circle = new Circle1(1);
+circle.location = { x: 1 };
 
-const another = new Circle(1);
+const propertyName = 'location';
 
 
 
-// adding/removing constructors
+circle['location']  = {y:2};
 
- 
-
+delete circle.location;
